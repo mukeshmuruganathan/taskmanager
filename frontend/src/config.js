a317ee92b759ca3658043324cd40d47187317b65
mukeshmuruganathan <mukeshmuruganathan28@gmail.com>
2026-02-20 22:12:16 +0530

    add

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,7 @@
 // API Configuration
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ;
+const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL ||
+  "https://taskmanager-1-4p9a.onrender.com";
 
 export default API_BASE_URL;
+
